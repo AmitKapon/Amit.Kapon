499e2c9f661796ba14826a8435955c3999e09e1d
Amit <amitkapon@gmail.com>
2021-05-08 21:54:38 +0300

    with json request

---
+++ CV/cvJSfile.js
@@ -0,0 +1,12 @@
+function applyStyles(element) {
+  element.style.backgroundColor = 'red';
+  element.style.fontSize = '50px'
+
+
+}
+
+function revertStyles(element) {
+  element.style.backgroundColor = 'white';
+  element.style.fontSize = '15px'
+}
+
